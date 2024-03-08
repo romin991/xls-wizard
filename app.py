cb3f7464f3753511b701a3807d244f6edb65075b
romin991 <romin991@gmail.com>
2024-03-08 20:46:59 +0700

    final commit

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import os
 from apikey import apikey 
 
 import streamlit as st 
-import openai
+from openai import OpenAI
+
+client = OpenAI(api_key=apikey)
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
@@ -14,9 +16,9 @@ from io import StringIO
 
 
 os.environ['OPENAI_API_KEY'] = apikey
-openai.api_key = apikey
 # App framework
-st.title('BISA YOUTUBE')
+st.title('XLS wizard')
+
 # file upload
 uploaded_file = st.file_uploader("Choose an Excel file", type=['xls', 'xlsx'])
 if uploaded_file is not None:
@@ -24,20 +26,12 @@ if uploaded_file is not None:
     # use Pandas to read the Excel file.
     df = pd.read_excel(uploaded_file)
     st.write(df)
-
-# prompt input
-prompt = st.text_input('Plug in your prompt here') 
-
-
-response = openai.Completion.create(
-  engine="text-davinci-003",
-  prompt=prompt,
-  temperature=0.7,
-  max_tokens=100,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0
-)
+    response = client.completions.create(
+    model="gpt-3.5-turbo-instruct",
+    prompt=f"Given the following data from an Excel file:\n{df.describe()} Only share the formula. Don't share anything else",
+    max_tokens=100
+    )
+    st.write(response.choices[0].text)
 
 
-st.write(response.choices[0].text)
+
